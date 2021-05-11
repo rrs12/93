7593feecddb34550b952163e9a71525ce9f21e77
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-05-11 14:05:11 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,5 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
       apiKey: "AIzaSyBB805nW41aCopPfz9E0e1f6p1zHyZOB28",
       authDomain: "chat-web-1.firebaseapp.com",
       databaseURL: "https://chat-web-1-default-rtdb.firebaseio.com",
@@ -7,21 +7,46 @@
       storageBucket: "chat-web-1.appspot.com",
       messagingSenderId: "677534835645",
       appId: "1:677534835645:web:ec1a5c461665c544e668d4"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
- //ADD YOUR FIREBASE LINKS HERE
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
 
- function getData() {
-       firebase.database().ref("/").on('value', function (snapshot) {
-             document.getElementById("output").innerHTML = "";
-             snapshot.forEach(function (childSnapshot) {
-                   childKey = childSnapshot.key;
-                   Room_names = childKey;
-                   //Start code
+user_name = localStorage.getItem("User Name");
+document.getElementById("welcome").innerHTML = "Welcome " + user_name + " !"
 
-                   //End code
-             });
-       });
- }
- getData();
+function addRoom() {
+      room_name = document.getElementById("room_name").value
+      firebase.database().ref("/").child(room_name).update({
+            Purpose: "Add Room"
+      });
+      localStorage.setItem("Room Name", room_name)
+      window.location = "kwitter_page.html"
+}
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) { //
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  //Start code
+                 console.log(Room_names)
+                  row = "<div class='room_name' id=" + Room_names + "onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>"
+                  document.getElementById("output").innerHTML+= row
+                  //End code
+            });
+      });
+}
+getData();
+                 function redirectToRoomName(name){
+                 console.log(name)
+                 localStorage.setItem("Room Name", name)
+                 window.location = "kwitter_page.html"
+                 }
+
+function logOut(){
+     localStorage.removeItem("User Name");
+     localStorage.removeItem("Room Name");
+     window.location="kwitter.html"
+}
